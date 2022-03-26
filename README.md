9711817a0030fe9df676ec2dc723fda76967740e
mp-gutierrez <96087439+mp-gutierrez@users.noreply.github.com>
2022-03-26 05:34:33 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+
+Title of the project
+    Bouncing PacMen Factory
+
+Description
+    A generator of same-sized PacMen that moves them at random speeds and directions. 
+
+
+Installation
+    In order to run the program, all the files need to be downloaded into the same folder, keeping each file name and its extension intact. With the files in place, a simple drag-n-drop of the index.html file into a chromium-based browser will load the game.
+
+Usage
+    The index.html has two buttons: "Add PacMan" and "Start Game".
+    "AddPacMan" adds a PacMan; the number is unlimited.
+    "Start game" makes the PacMen move around the browser window. If clicked more than once, the velocity of each generated PacMan increases.
+    Refreshing the page restarts the game.
+
+Support
+    In case of requiring further assistance, you can contact me via Twitter @mpgutierrz
+
+Improvements to come
+    Adding noise to the chomps of each PacMan
+    Make each PacMan bite continously
+
+License
+    This project is licensed under the MIT License.
---
+++ pacmen.js
@@ -0,0 +1,81 @@
+let pos = 0;
+const pacArray = [
+  ['./images/PacMan1.png', './images/PacMan2.png'],
+  ['./images/PacMan3.png', './images/PacMan4.png']
+];
+
+let direction = 0;
+const pacMen = [];
+
+function setToRandom(scale) {
+  return {
+    x: Math.random() * scale,
+    y: Math.random() * scale,
+  };
+}
+// Factory to make a PacMan 
+function makePac() {
+  // returns an object with values scaled {x: 33, y: 21}
+  let velocity = setToRandom(10);
+  let position = setToRandom(200);
+  // Add image to div id = game
+  let game = document.getElementById('game');
+  let newimg = document.createElement('img');
+  newimg.style.position = 'absolute';
+  newimg.src = './images/PacMan1.png';
+  newimg.width = 100;
+  newimg.style.left = position.x;
+  newimg.style.top = position.y;
+  game.appendChild(newimg);
+  // new style of creating an object
+  return {
+    position,
+    velocity,
+    newimg,
+  };
+}
+
+function update() {
+  //loop over pacmen array and move each one and move image in DOM
+  pacMen.forEach((item) => {
+    checkCollisions(item)
+      item.position.x += item.velocity.x;
+      item.position.y += item.velocity.y;
+
+      item.newimg.style.left = item.position.x;
+      item.newimg.style.top = item.position.y;
+    });
+  setTimeout(update, 20);
+}
+
+// Detects collision with all walls and makes pacman bounce and bite after a change of direction
+function checkCollisions(item) {
+  if(item.position.x + item.velocity.x + item.newimg.width > window.innerWidth) {
+    item.velocity.x = -item.velocity.x;
+    item.newimg.src = 'images/PacMan3.png';
+  }
+  if(item.position.x + item.velocity.x < 0) {
+    item.velocity.x = -item.velocity.x;
+    item.newimg.src = 'images/PacMan1.png';
+  }
+  if(item.position.y + item.velocity.y + item.newimg.height > window.innerHeight && item.velocity.x > 0) {
+    item.velocity.y = -item.velocity.y;
+    item.newimg.src = 'images/PacMan2.png'; 
+  }
+  if(item.position.y + item.velocity.y + item.newimg.height > window.innerHeight && item.velocity.x < 0) {
+    item.velocity.y = -item.velocity.y;
+    item.newimg.src = 'images/PacMan4.png'; 
+  }
+  if(item.position.y + item.velocity.y < 0 && item.velocity.x > 0) {
+    item.velocity.y = -item.velocity.y;
+    item.newimg.src = 'images/PacMan2.png'; 
+  }
+  if(item.position.y + item.velocity.y < 0 && item.velocity.x < 0) {
+    item.velocity.y = -item.velocity.y;
+    item.newimg.src = 'images/PacMan4.png'; 
+  }
+}
+
+function makeOne() {
+  pacMen.push(makePac()); // add a new PacMan
+}
